f4582f5a182a3aa05c88f45fe861490810554600
Youngseo Yoo <7dudtj@naver.com>
2021-09-04 14:41:10 +0900

    Fix minor error

--- CoinAutoTrader/cat_v.1.0.1.py
+++ CoinAutoTrader/cat_v.1.0.1.py
@@ -102,7 +102,7 @@ buy_price = 0
 sell_price = 0
 time.sleep(0.2)
 post_message(myToken, "#coin", "Start CAT!\n"+str(now))
-post_message(myToken, "#coin", "Currrent money: "+str(money)+"won")
+post_message(myToken, "#coin", "Currrent money: "+str(int(money))+"won")
 
 
 # set tickers information
