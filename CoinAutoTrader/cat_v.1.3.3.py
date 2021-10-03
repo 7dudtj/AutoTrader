799483332bb62e6abcbf2e5c5d9659042b65e83f
Youngseo Yoo <7dudtj@naver.com>
2021-10-03 12:30:23 +0900

    change some values

--- CoinAutoTrader/cat_v.1.3.3.py
+++ CoinAutoTrader/cat_v.1.3.3.py
@@ -41,7 +41,7 @@ def set_tickers(ticker, k):
 
     # find dangerous situation
     range = 0.1
-    if df.iloc[-2]['high'] * range <= (df.iloc[-2]['high'] - df.iloc[-2]['low']):
+    if df.iloc[-2]['low'] * range <= (df.iloc[-2]['high'] - df.iloc[-2]['low']):
         danger = True
 
     # return [target, attain(False), danger]
@@ -91,7 +91,7 @@ tickers = {}
 now = datetime.datetime.now() + datetime.timedelta(hours=9) # change hours by your server time
 buy_time = now
 upbit = pyupbit.Upbit(access, secret)
-k = 0.7 # low k: high risk, high return
+k = 0.5 # low k: high risk, high return
 current_ticker = ""
 current_open = 0
 buy = False
@@ -102,7 +102,7 @@ money = get_balance("KRW")
 buy_price = 0
 sell_price = 0
 time.sleep(0.2)
-post_message(myToken, "#coin", "Start CAT_v.1.3.2!\n"+str(now))
+post_message(myToken, "#coin", "Start CAT_v.1.3.3!\n"+str(now))
 post_message(myToken, "#coin", "Currrent money: "+str(int(money))+"won")
 
 
@@ -136,7 +136,7 @@ while True:
                     tickers[ticker][1] = True
                     # buy: false & not dangerous & after 09:05:00 >> do buy
                     if buy is False and today_buy is True and tickers[ticker][2] is False \
-                            and now >= start_time + datetime.timedelta(minutes=5):
+                            and now >= start_time + datetime.timedelta(minutes=10):
                         krw = get_balance("KRW")
                         buy_price = current_price
                         upbit.buy_market_order(ticker, krw*0.9995) # buy: market order
