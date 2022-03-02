195a2e231bebdfab51bccdd34d85e0c742a66023
Youngseo Yoo <7dudtj@naver.com>
2022-03-02 18:13:41 +0900

    Synchronize with ver 1.2.1

--- StockAutoTrader/sat_test.py
+++ StockAutoTrader/sat_test.py
@@ -341,10 +341,6 @@ if __name__ == '__main__':
             t_sell = t_now.replace(hour=15, minute=15, second=0, microsecond=0)
             t_exit = t_now.replace(hour=15, minute=20, second=0, microsecond=0)
             today = datetime.today().weekday()
-            # Sat or Sun >> End program
-            if today == 5 or today == 6:
-                printlog('Today is', 'Saturday.' if today == 5 else 'Sunday.')
-                endProgram()
             # sell unsold items before trading start
             if t_9 < t_now < t_start and soldout == False: 
                 soldout = True
